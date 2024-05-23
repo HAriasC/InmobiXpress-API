cf56c8c20f4c91eb72c85e552d625f853856c5b0
Henry Arias <h.arias.c3@gmail.com>
2024-05-23 00:44:05 -0500

    Cambio del nombre del paquete principal.

--- src/test/kotlin/com/inmobiXpress/ApplicationTest.kt
+++ src/test/kotlin/com/inmobiXpress/ApplicationTest.kt
@@ -1,6 +1,6 @@
-package com.example
+package com.inmobiXpress
 
-import com.example.plugins.*
+import com.inmobiXpress.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
